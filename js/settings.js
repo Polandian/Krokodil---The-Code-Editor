90eab4795cdf0089b37b59519be0c1c59ed08db2
Polandian <101973563+Polandian@users.noreply.github.com>
2024-11-12 14:17:32 +0300

    relative lines update

--- js/settings.js
+++ js/settings.js
@@ -1,8 +1,10 @@
 let relativeLines = {name: "relativeLines", value: null}
 let editorSwing = {name:"editorSwing", value: null}
 let cursorBlink = {name:"cursorBlink", value: null}
+let editorTransparent = {name:"editorTransparent", value: null}
+let behindCaret = {name: "behindCaret", value: null}
 
-const allSetting = [relativeLines, editorSwing, cursorBlink]
+const allSetting = [relativeLines, editorSwing, cursorBlink, editorTransparent, behindCaret]
 
 function loadAnims(){
 	if(editorSwing.value == true) document.getElementById("codeEditor").classList.add("codeEditorAnim")
@@ -10,12 +12,25 @@ function loadAnims(){
 
 	if(cursorBlink.value == true) document.getElementById("editorCursor").classList.add("editorCursorAnim") 
 	else document.getElementById("editorCursor").classList.remove("editorCursorAnim")
+
+	if(editorTransparent.value == true) document.getElementById("codeEditor").style.background = "none"
+	else document.getElementById("codeEditor").style.background = "rgb(20, 20, 20)"
+	
+
+	if(behindCaret.value == true) document.getElementById("editorCursor").classList.add("editorCursorBlend")
+	else document.getElementById("editorCursor").classList.remove("editorCursorBlend")
 }
 
 function changeSetting(y){ //change setting
 	localStorage.setItem(y, document.getElementById(y).checked)
 	eval(y).value = (localStorage.getItem(y) === "true")
 	loadAnims()
+
+	if(y == "relativeLines" && relativeLines.value == false){ //if relativeLines are closed, sets line numbers in a correct order
+		for(var i = 0; i < document.getElementById("lineNumber").childElementCount; i++){
+			document.querySelectorAll(".lineNumber")[i].innerText = i + 1
+		}
+	}
 }
 
 for(var i = 0; i < allSetting.length; i++){ //loads all settings
